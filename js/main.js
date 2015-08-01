d740f0f44c992722057cdf581aad301aa8a964c3
johnny lamb <agnusjack@gmail.com>
2015-08-01 14:15:57 -0600

    third commit

--- js/main.js
+++ js/main.js
@@ -8,9 +8,9 @@ $(document).on('ready', function() {
 
     var $dollarAmount = $('#dollar-amount').val();
     var $tipPercent = $('#tip-percent').val();
-    var result = ($dollarAmount * $tipPercent).toFixed(2);
+    var answer = ($dollarAmount * $tipPercent).toFixed(2);
 
-    $('#result').html(result + " should be how much you tip.");
+    $('#result').html(answer + " should be how much you tip.");
 
   });
 
