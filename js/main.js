6fa9ed27f50f65d18d738e3ba41cbec9d965bb54
johnny lamb <agnusjack@gmail.com>
2015-08-01 14:14:03 -0600

    second commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 // add scripts
 
 $(document).on('ready', function() {
-  console.log('sanity check!');
+
+
+  $('#calculate').on('click', function(event){
+    event.preventDefault();
+
+    var $dollarAmount = $('#dollar-amount').val();
+    var $tipPercent = $('#tip-percent').val();
+    var result = ($dollarAmount * $tipPercent).toFixed(2);
+
+    $('#result').html(result + " should be how much you tip.");
+
+  });
+
+
 });
+
